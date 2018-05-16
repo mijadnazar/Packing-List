22bd31cfe9c91c43ac8419b385f47c2cc4132a45
mehdi ijadnazar <ijadnazar.mahdi@gmail.com>
2018-05-16 20:59:21 +0430

    Animated height constraint

--- PackingList/ViewController.swift
+++ PackingList/ViewController.swift
@@ -29,6 +29,8 @@ class ViewController: UIViewController {
   @IBOutlet var tableView: UITableView!
   @IBOutlet var buttonMenu: UIButton!
   @IBOutlet var titleLabel: UILabel!
+
+    @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
   
   //MARK: further class variables
   
@@ -39,7 +41,16 @@ class ViewController: UIViewController {
   //MARK: class methods
   
   @IBAction func actionToggleMenu(_ sender: AnyObject) {
-    
+    isMenuOpen = !isMenuOpen
+    menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+    titleLabel.text = isMenuOpen ? "Select Item" : "Packing List"
+
+    UIView.animate(withDuration: 1.0, delay: 0.0,
+                   usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0,
+                   options: .curveEaseIn,
+                   animations: {
+                    self.view.layoutIfNeeded()
+    }, completion: nil)
   }
   
   func showItem(_ index: Int) {
